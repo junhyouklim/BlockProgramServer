98907d16055d78b4049e2cbe7aaec93ba3f67c13
junhyouk lim <jhlim6926@gmail.com>
2020-10-20 20:42:44 +0900

    변경

--- BlockProgramServer/MainForm.Designer.cs
+++ BlockProgramServer/MainForm.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnServOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnServOpen
+            // 
+            this.btnServOpen.Location = new System.Drawing.Point(180, 74);
+            this.btnServOpen.Name = "btnServOpen";
+            this.btnServOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnServOpen.TabIndex = 0;
+            this.btnServOpen.Text = "Open";
+            this.btnServOpen.UseVisualStyleBackColor = true;
+            this.btnServOpen.Click += new System.EventHandler(this.btnServOpen_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnServOpen);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnServOpen;
     }
 }
 
--- BlockProgramServer/MainForm.cs
+++ BlockProgramServer/MainForm.cs
@@ -16,5 +16,11 @@ namespace BlockProgramServer
         {
             InitializeComponent();
         }
+
+        private void btnServOpen_Click(object sender, EventArgs e)
+        {
+            //서버 오픈 버튼
+
+        }
     }
 }

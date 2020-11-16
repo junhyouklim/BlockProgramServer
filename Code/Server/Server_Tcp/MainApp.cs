40136947b0b0a0431ade7fafe6bdbd8d8874fe91
JunHyouk-Lim <40488563+junhyouklim@users.noreply.github.com>
2020-11-16 23:44:19 +0900

    Add files via upload

---
+++ Code/Server/Server_Tcp/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Server_Tcp
+{
+    class MainApp
+    {
+        private static void Main(string[] args)
+        {
+            Server_Tcp server = new Server_Tcp(9043);
+            server.ServerOn();
+        }
+    }
+}
